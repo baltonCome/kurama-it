14e7d7fe9d41a8fb5da469fb75c083739ee18b58
baltonCome <baltoncome@gmail.com>
2021-12-10 08:13:22 +0200

    usuario

---
+++ database/migrations/2021_12_09_213255_create_usuarios_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('username');
+            $table->binary('image')->nullable();
+            $table->text('headline')->nullable();
+            $table->string('country')->nullable();
+            $table->string('city')->nullable();
+            $table->text('skills')->nullable();
+            $table->text('languages')->nullable();
+            $table->text('experience')->nullable();
+            $table->text('education')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
